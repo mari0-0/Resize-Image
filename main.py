2b388cf5d3450be584adc0abd59124366fb123b2
mari0-0 <abhainike@gmail.com>
2024-08-19 02:25:52 +0530

    vercel deployment

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ from flask import Flask, request, render_template, send_file
 import os
 from resizer import resize_image
 
-
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-RESIZED_FOLDER = 'resized'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['RESIZED_FOLDER'] = RESIZED_FOLDER
 
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(RESIZED_FOLDER, exist_ok=True)
+# Create /tmp if it doesn't exist for LOCAL MACHINE ONLY
+# os.makedirs('/tmp', exist_ok=True)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,13 +20,13 @@ def index():
         
         if file:
             filename = file.filename
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file_path = os.path.join('/tmp', filename)  # Save the uploaded file to /tmp
             file.save(file_path)
             
             width = int(request.form['width'])
             height = int(request.form['height'])
             resized_filename = f"resized_{filename}"
-            resized_path = os.path.join(app.config['RESIZED_FOLDER'], resized_filename)
+            resized_path = os.path.join('/tmp', resized_filename)  # Save the resized image to /tmp
             
             resize_image(file_path, resized_path, width, height)
             
@@ -40,7 +36,7 @@ def index():
 
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_file(os.path.join(app.config['RESIZED_FOLDER'], filename), as_attachment=True)
+    return send_file(os.path.join('/tmp', filename), as_attachment=True)  # Serve the file from /tmp
 
 if __name__ == '__main__':
     app.run()
